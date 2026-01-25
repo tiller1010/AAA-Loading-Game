dedd06348552f0619cb0dff29805a4debcc6a6f6
Tyler <tiller1010@gmail.com>
2026-01-24 19:15:59 -0500

    feat: basic movement

---
+++ Assets/Scripts/Player/PlayerControls.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerControls : MonoBehaviour
+{
+    //[SerializeField] Transform target;
+
+    private float moveSpeed = 60.0f;
+
+    InputAction moveAction;
+
+    private CharacterController characterController;
+
+    void Start()
+    {
+        moveAction = InputSystem.actions.FindAction("Move");
+
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        Vector3 movement = Vector3.zero;
+
+        //float horizontalInput = Input.GetAxis("Horizontal");
+        //float verticalInput = Input.GetAxis("Vertical");
+
+        //Debug.Log(horizontalInput);
+
+        Vector2 moveValue = moveAction.ReadValue<Vector2>();
+
+        //Debug.Log(moveValue);
+
+        movement = movement.normalized;
+        movement.x = -moveValue.x;
+        movement.z = -moveValue.y;
+
+        movement *= moveSpeed;
+        movement *= Time.deltaTime;
+
+        characterController.Move(movement);
+    }
+}
