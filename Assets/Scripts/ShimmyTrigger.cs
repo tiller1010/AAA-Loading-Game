7b92442b807766489439e5c4715100b789f73169
Tyler <tiller1010@gmail.com>
2026-02-24 08:55:23 -0500

    feat: enter tunnel at either end

--- Assets/Scripts/ShimmyTrigger.cs
+++ Assets/Scripts/ShimmyTrigger.cs
@@ -8,25 +8,28 @@ public class ShimmyTrigger : MonoBehaviour
         if (gameObject.tag == "Player")
         {
             Animator animator = gameObject.GetComponent<Animator>();
-            animator.SetBool("Shimmying", true);
-
-            PlayerControls playerControls = gameObject.GetComponent<PlayerControls>();
-            playerControls.SetMoveSpeed(1f);
-            playerControls.SetRotationSpeed(0f);
-            playerControls.SetRotation(transform.rotation);
-
-            Vector3 shimmyStartPosition = transform.TransformPoint(Vector3.back / 2f);
-            shimmyStartPosition.y = .71f; // Set character y position
-
-            playerControls.SetShimmyStartPosition(shimmyStartPosition);
-
-            GameObject camera = GameObject.Find("Main Camera");
-            if (camera != null)
+            if (!animator.GetBool("Shimmying"))
             {
-                CameraOrbit cameraOrbit = camera.GetComponent<CameraOrbit>();
-                cameraOrbit.SetShimmyLocked(true);
-                cameraOrbit.SetShimmyLockRotation(transform.rotation.eulerAngles.y);
+                PlayerControls playerControls = gameObject.GetComponent<PlayerControls>();
+                playerControls.SetMoveSpeed(1f);
+                playerControls.SetRotationSpeed(0f);
+                playerControls.SetRotation(transform.rotation);
+
+                Vector3 shimmyStartPosition = transform.TransformPoint(Vector3.back / 2f);
+                shimmyStartPosition.y = .71f; // Set character y position
+
+                playerControls.SetShimmyStartPosition(shimmyStartPosition);
+
+                GameObject camera = GameObject.Find("Main Camera");
+                if (camera != null)
+                {
+                    CameraOrbit cameraOrbit = camera.GetComponent<CameraOrbit>();
+                    cameraOrbit.SetShimmyLocked(true);
+                    cameraOrbit.SetShimmyLockRotation(transform.rotation.eulerAngles.y);
+                }
             }
+
+            animator.SetBool("Shimmying", true);
         }
     }
 
