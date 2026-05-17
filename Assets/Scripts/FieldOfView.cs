e5ebb58f242a09200fd4c9c59172330324bc1f2b
Tyler <tiller1010@gmail.com>
2026-05-17 11:13:57 -0400

    feat: enemy field of view

---
+++ Assets/Scripts/FieldOfView.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class FieldOfView : MonoBehaviour
+{
+    public float viewRadius;
+    [Range(0, 360)]
+    public float viewAngle;
+
+    public Vector3 DirFromAngle(float angleInDegrees, bool angleIsGlobal)
+    {
+        if (!angleIsGlobal)
+        {
+            angleInDegrees += transform.eulerAngles.y;
+        }
+        return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), 0, Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
+    }
+}
