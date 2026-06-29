7ed3a1672a3739b54b9a8f8e72f1cfb8e5ae3923
Tyler <tiller1010@gmail.com>
2026-06-29 08:47:39 -0400

    feat: game over text

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -2,11 +2,16 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using System.Text.RegularExpressions;
 using UnityEngine.UI;
+using TMPro;
 
 public class GameController : MonoBehaviour
 {
     [SerializeField] private Text healthLabel;
     [SerializeField] private Slider healthSlider;
+    [SerializeField] private GameObject gameOverText;
+
+    private float hudVerticalCenter;
+    private bool isGameOver = false;
 
     void Awake()
     {
@@ -27,6 +32,27 @@ public class GameController : MonoBehaviour
     {
         QualitySettings.vSyncCount = 0;
         Application.targetFrameRate = 59;
+
+        gameOverText.SetActive(false);
+        hudVerticalCenter = Screen.height / 2 + 43; // Plus text height (43)
+    }
+
+    void FixedUpdate()
+    {
+        if (isGameOver && gameOverText.transform.position.y != hudVerticalCenter)
+        {
+            float gameOverTextPositionY = gameOverText.transform.position.y - 5;
+            if (gameOverTextPositionY < hudVerticalCenter)
+            {
+                gameOverTextPositionY = hudVerticalCenter;
+            }
+            Vector3 gameOverTextPosition = new Vector3(
+                gameOverText.transform.position.x,
+                gameOverTextPositionY,
+                gameOverText.transform.position.z
+            );
+            gameOverText.transform.position = gameOverTextPosition;
+        }
     }
 
     public static void NextLevel()
@@ -49,5 +75,11 @@ public class GameController : MonoBehaviour
         string message = "Health: " + newHealth;
         healthLabel.text = message;
         healthSlider.value = newHealth;
+
+        if (newHealth == 0)
+        {
+            gameOverText.SetActive(true);
+            isGameOver = true;
+        }
     }
 }
