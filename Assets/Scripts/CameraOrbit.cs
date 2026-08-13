541ba736f5dba89dfad8a00b0d9649d5b362d4b9
Tyler <tiller1010@gmail.com>
2026-08-13 19:06:04 -0400

    wip cleanup and calculate new position before setting

--- Assets/Scripts/CameraOrbit.cs
+++ Assets/Scripts/CameraOrbit.cs
@@ -12,7 +12,6 @@ public class CameraOrbit : MonoBehaviour
     private Vector3 offset;
     private Vector3 originalOffset;
     private float originalDistanceFromPlayer;
-    private int iterations = 0;
     private GameObject obstructingObject;
 
     InputAction moveAction;
@@ -89,18 +88,14 @@ public class CameraOrbit : MonoBehaviour
         }
         else
         {
-            transform.position = newPosition;
-            transform.LookAt(target);
-
-            CheckForObstructions();
+            CheckForObstructions(newPosition);
             if (obstructingObject != null)
             {
-                float distanceFromPlayer = Vector3.Distance(transform.position, target.position);
-                float distanceFromObstructingObject = Vector3.Distance(transform.position, obstructingObject.transform.position);
-                //float averageObstructionSize = (obstructingObject.transform.localScale.x + obstructingObject.transform.localScale.y + obstructingObject.transform.localScale.z) / 3f;
+                float distanceFromPlayer = Vector3.Distance(newPosition, target.position);
+                float distanceFromObstructingObject = Vector3.Distance(newPosition, obstructingObject.transform.position);
 
                 Collider[] colliders = Physics.OverlapSphere(
-                    transform.position,
+                    newPosition,
                     0.01f
                 );
                 bool isInObstruction = colliders.Length > 0;
@@ -108,82 +103,38 @@ public class CameraOrbit : MonoBehaviour
                 Debug.Log("Distance from player: " + distanceFromPlayer);
                 Debug.Log("Distance from obstruction: " + distanceFromObstructingObject);
                 Debug.Log("camera is in a collider: " + isInObstruction);
-                // if (distanceFromObstructingObject - averageObstructionSize < distanceFromPlayer)
                 if ((distanceFromObstructingObject < distanceFromPlayer || isInObstruction) && distanceFromPlayer > .5f)
                 {
-                    //offset.x -= Time.deltaTime;
-                    //offset.z -= Time.deltaTime;
-                    //offset.y -= Time.deltaTime;
-
-                    //transform.position = Vector3.MoveTowards(transform.position, target.position, 5 * Time.deltaTime);
-                    //while (IsObstructedByGameWorld() && distanceFromPlayer > 1f && iterations < 100)
-                    //{
-                    //    Debug.Log("is obstructed");
-                    //    // Move camera towards player
                     Vector3 playerCenter = target.position;
                     playerCenter.y += 1;
-                    transform.position = Vector3.MoveTowards(transform.position, playerCenter, Time.deltaTime);
-                    offset = target.position - transform.position;
-                    offset.x = Mathf.Abs(offset.x);
-                    offset.z = Mathf.Abs(offset.z);
-                    //  iterations++;
-                    //}
-                    //iterations = 0;
+                    newPosition = Vector3.MoveTowards(newPosition, playerCenter, Time.deltaTime);
+                    //offset = target.position - transform.position;
+                    //offset.x = Mathf.Abs(offset.x);
+                    //offset.z = Mathf.Abs(offset.z);
                 }
                 else if (Mathf.Abs(offset.x) < originalOffset.x || Mathf.Abs(offset.z) < originalOffset.z)
                 {
                     // move away from player until the original offset is reached
-                    //Vector3 originalPositionByOffset = target.position - (Quaternion.Euler(rotationX, rotationY, 0) * originalOffset);
-                    //transform.position = Vector3.MoveTowards(transform.position, originalPositionByOffset, Time.deltaTime);
+                    //Vector3 originalPositionByOffset = newPosition - (Quaternion.Euler(rotationX, rotationY, 0) * originalOffset);
+                    //newPosition = Vector3.MoveTowards(newPosition, originalPositionByOffset, Time.deltaTime);
                     //offset = target.position - transform.position;
                     //offset.x = Mathf.Abs(offset.x);
                     //offset.z = Mathf.Abs(offset.z);
-
-                    //if (offset.x < originalOffset.x)
-                    //{
-                    //    offset.x += Time.deltaTime;
-                    //}
-                    //if (offset.z < originalOffset.z)
-                    //{
-                    //    offset.z += Time.deltaTime;
-                    //}
-                    //if (offset.y < originalOffset.y)
-                    //{
-                    //    offset.y += Time.deltaTime;
-                    //}
-                    //offset.x = originalOffset.x;
-                    //offset.z = originalOffset.z;
                 }
-                //else
-                //{
-                //    while (!IsObstructedByGameWorld() && distanceFromPlayer < originalDistanceFromPlayer && iterations < 100)
-                //    {
-                //        // Move camera away from player
-                //      transform.position = Vector3.MoveTowards(transform.position, target.position - (Quaternion.Euler(rotationX, rotationY, 0) * offset), 5 * Time.deltaTime);
-                //      iterations++;
-                //    }
-                //    iterations = 0;
-                //}
             }
 
         }
+
+        transform.position = newPosition;
+        transform.LookAt(target);
     }
 
-    public void CheckForObstructions()
+    public void CheckForObstructions(Vector3 newPosition)
     {
         RaycastHit obstacleCheck;
-        Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out obstacleCheck);
+        Physics.Raycast(newPosition, transform.TransformDirection(Vector3.forward), out obstacleCheck);
         if (!obstacleCheck.collider) return;
         obstructingObject = obstacleCheck.collider.gameObject;
-    }
-
-    public bool IsObstructedByGameWorld()
-    {
-        RaycastHit obstacleCheck;
-        Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out obstacleCheck);
-        if (!obstacleCheck.collider) return false;
-        Debug.Log("camera obstruction: " + obstacleCheck.collider.gameObject.name);
-        return obstacleCheck.collider.gameObject.CompareTag("GameWorld");
     }
 
     public void SetShimmyLocked(bool shimmyLock)
